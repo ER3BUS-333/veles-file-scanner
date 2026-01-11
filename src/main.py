3946d7e8a4b464be6110733edc54f53eb6af5aa2
ER3BUS <korzh_y@inbox.ru>
2026-01-11 18:21:22 +0300

    list files in selected directory

--- src/main.py
+++ src/main.py
@@ -1,6 +1,27 @@
+from pathlib import Path 
+
 def main():
+    
     print("Veles запущен")
     user_inpt = input("Введите путь к папке с файлами:  ")
     print("Вы ввели: ",user_inpt)
+
+    p = Path(user_inpt)
+    if not p.exists():
+        print("Ошибка!: путь не существует")
+        return
+    if not p.is_dir():
+        print("Ошибка!: это не папка")
+        return
+    
+    print("Всё ок папка существует и это папка")
+    
+    print("Файлы в папке: ")
+
+    for item in p.iterdir():
+        if item.is_file():
+            print(item)
+
+
 if __name__ == "__main__":
     main()
